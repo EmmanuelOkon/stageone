84e11efc0cc9e8ea542f3f246eea73cb3e8a8377
Emmanuel Okon <emmaudo27@gmail.com>
2023-09-07 16:52:12 +0100

    minor fixes

---
+++ src/script.js
@@ -0,0 +1,35 @@
+function updateTime() {
+  const currentDayOfTheWeekElement = document.querySelector(
+    '[data-testid="currentDayOfTheWeek"]'
+  );
+
+  const currentUTCTimeElement = document.querySelector(
+    '[data-testid="currentUTCTime"]'
+  );
+
+  const currentDate = new Date();
+
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const currentDayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+
+  const currentUTCHours = currentDate.getUTCHours();
+  const currentUTCMinutes = currentDate.getUTCMinutes();
+  const currentUTCSeconds = currentDate.getUTCSeconds();
+  const currentTimeFormat = currentUTCHours >= 12 ? "PM" : "AM";
+  const formattedUTCHours = currentUTCHours % 12 || 12;
+  const currentUTCTime = `${formattedUTCHours}:${currentUTCMinutes.toString().padStart(2, '0')} ${currentTimeFormat} Local Time`;
+
+  currentDayOfTheWeekElement.textContent = currentDayOfWeek;
+  currentUTCTimeElement.textContent = currentUTCTime;
+}
+updateTime();
+setInterval(updateTime, 60000);
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  
+  theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      raleway: ["Raleway", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
